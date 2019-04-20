2e9a6dbb5642e578c93ab190784eebe6e8c3934b
Masum Billah <49682505+drbillah@users.noreply.github.com>
2019-04-20 18:34:36 +0600

    Update gatk4.sh

--- gatk4.sh
+++ gatk4.sh
@@ -11,21 +11,40 @@ export sample=$5
 export group=1
 
 mkdir ${current_path}/"index"
+
+#Extract reference path
+foo=$ref
+for (( i=0; i<${#foo}; i++ )); do char="${foo:$i:1}"
+if [ "$char" == "/" ]; then pos=$i; fi
+if [ "$char" == "." ]; then dpos=$i; fi
+done
+
+interval=$((dpos-pos-1))
+refName=${foo:pos+1:$interval}
+dir=${foo:0:pos}
+
+echo `${samtools} faidx $ref`
+
+if [ -z "$dir" ]; then 
+  ${gatk4} CreateSequenceDictionary -R $ref -O $refName.dict
+else
+  ${gatk4} CreateSequenceDictionary -R $ref -O $dir/$refName.dict
+fi
+
 bigRef="bwtsw"
 smallRef="is"
 threshold=2000000000
 refsize=$(wc -c < $ref)
 
 if [ $refsize -ge $threshold ]; then
-    echo `$bwa index -p index/ref -a $bigRef `
+    echo `${bwa} index -p index/ref -a $bigRef `
 else
-    echo `$bwa index -p index/ref -a $smallRef $ref`
+    echo `${bwa} index -p index/ref -a $smallRef $ref`
 fi
-echo `$samtools faidx $ref`
-echo `${gatk4} CreateSequenceDictionary -R ref.fa -O ref.dict`
 
-echo `$bwa mem -t $threads -M -R '@RG\tID:$group\tLB:$group\tPL:ILLUMINA\tSM:$sample' index/ref $fastq1 $fastq2 | samtools view -b -S -o $sample.bam`
-echo `${gatk4} --java-options "-Xmx1G" SortSam -I ${current_path}/$sample.bam -O ${current_path}/$sample.sorted.bam -SO coordinate --CREATE_INDEX true`
-echo `${gatk4} --java-options "-Xmx1G" MarkDuplicates -I ${current_path}/$sample.sorted.bam -O ${current_path}/$sample.dedup.bam -M ${current_path}/$sample.metrics --REMOVE_DUPLICATES true --CREATE_INDEX true`
-echo `rm -rf ${current_path}/$sample.bam ${current_path}/$sample.sorted.bam ${current_path}/$sample.sorted.bai`
-echo `${gatk4} --java-options "-Xmx1G" HaplotypeCaller -R $ref -I ${current_path}/$sample.dedup.bam -O ${current_path}/gatk4.raw.vcf`
+echo `${bwa} mem -t $threads -M -R '@RG\tID:$group\tLB:$group\tPL:ILLUMINA\tSM:$sample' index/ref $fastq1 $fastq2 | samtools view -b -S -o $sample.bam`
+echo `${gatk4} --java-options "-Xmx1G" SortSam -I $sample.bam -O $sample.sorted.bam -SO coordinate --CREATE_INDEX true`
+echo `${gatk4} --java-options "-Xmx1G" MarkDuplicates -I $sample.sorted.bam -O $sample.dedup.bam -M $sample.metrics --REMOVE_DUPLICATES true --CREATE_INDEX true`
+echo `rm -rf $sample.bam $sample.sorted.bam $sample.sorted.bai`
+echo `${gatk4} --java-options "-Xmx1G" HaplotypeCaller -R $ref -I $sample.dedup.bam -O gatk4.raw.vcf`
+echo $dir
